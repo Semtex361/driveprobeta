fafd454938ef60018877c9c7480a413bece78ade
Semtex361 <furki-turki@hotmail.de>
2026-08-18 00:31:09 +0200

    Delete STARTEN.bat

--- STARTEN.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if not exist "%~dp0Fahrschul_Tracker_Windows.html" (
- echo Fehler: HTML-Datei fehlt. Bitte ZIP komplett entpacken.
- pause
- exit /b 1
-)
-start "" "%~dp0Fahrschul_Tracker_Windows.html"
