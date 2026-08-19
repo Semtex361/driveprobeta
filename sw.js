bd3243a827500eb81feea3081b70b8625b9d0361
Semtex361 <furki-turki@hotmail.de>
2026-08-19 22:04:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='drivepro-beta-v90-6-reliable-sync';
+const CACHE='drivepro-beta-v90-8-diagnostic-sync';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
