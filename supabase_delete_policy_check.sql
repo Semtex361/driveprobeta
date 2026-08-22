e679d8ada91e49ebe266b5449db1b45ea9c7ee80
Semtex361 <furki-turki@hotmail.de>
2026-08-22 21:27:53 +0200

    Delete supabase_delete_policy_check.sql

--- supabase_delete_policy_check.sql
+++
@@ -1,6 +0,0 @@
--- Optional diagnostic only: run if DrivePro reports that a lesson could not be deleted.
--- It shows the current DELETE policies on lessons.
-select policyname, cmd, roles, qual, with_check
-from pg_policies
-where schemaname='public' and tablename='lessons'
-order by policyname;
